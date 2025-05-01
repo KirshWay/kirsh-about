5e4831c260d710fddfb0fb1ab0b70321993fc05d
Viktor Nemets <nemecvictor.kirsh@gmail.com>
2025-05-01 13:57:54 +0300

    feat: adding new styles and components, updating Tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,25 @@ export default {
         sans: ['Geist Mono', 'ui-sans-serif', 'system-ui', 'sans-serif'],
         mono: ['JetBrains Mono', 'ui-monospace', 'SFMono-Regular', 'monospace'],
       },
+      colors: {
+        background: 'var(--color-background)',
+        foreground: 'var(--color-foreground)',
+        
+        'text-primary': 'var(--color-text-primary)',
+        'text-secondary': 'var(--color-text-secondary)',
+        
+        'accent-primary': 'var(--color-accent-primary)',
+        'accent-primary-hover': 'var(--color-accent-primary-hover)',
+        'accent-secondary': 'var(--color-accent-secondary)',
+        'accent-secondary-hover': 'var(--color-accent-secondary-hover)',
+        
+        'component-primary': 'var(--color-component-primary)',
+        'component-primary-hover': 'var(--color-component-primary-hover)',
+        'component-secondary': 'var(--color-component-secondary)',
+        
+        'border': 'var(--color-border)',
+        'border-hover': 'var(--color-border-hover)',
+      },
     },
   },
   plugins: [],
